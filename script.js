d749eacd23cf3d2f1e7b7ab34d60f7b6965363fc
Shashankmanju <99338637+Shashankmanju@users.noreply.github.com>
2024-02-28 23:00:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed(".text", {
+        strings: ["Programming", "Web Development"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
+var sidemenu = document.getElementById("sidemenu");
+
+    function openmenu() {
+        sidemenu.style.right = "0";
+    }
+
+    function closemenu() {
+        sidemenu.style.right = "-200px";
+    }
